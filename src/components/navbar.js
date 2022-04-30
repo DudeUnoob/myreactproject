2a616d174d21c864a5f6aad1fc742e285b9749f0
DudeUnoob <to.baladev@gmail.com>
2022-04-29 20:08:43 -0500

    update on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,31 @@
 import App from "../App";
 import React from "react";
+import '../public/Home.css';
 
 
-const styling ={
-    color:'blue',
-    
+const styling = {
+  color: 'blue',
+
 }
 
 let myArray = ['testin', 'blogs']
 class Navbar extends React.Component {
-   
-    render() {
-        
-        return (
-            <ul>
-              {myArray.map(animal => (
-                
-                <li>
-                <a href={animal} onClick={() => window.open(`/${animal}`, "_blank")}>{animal}</a>
-                </li>
-              ))}
-            </ul>
-          );
-    }
+
+  render() {
+
+    return (
+      <>
+        <div class="topnav">
+          <a class="active" href="/">Home</a>
+          {myArray.map(elm => (
+            <a href={elm}>{elm}</a>
+          ))}
+        </div>
+        <h1 id="home">Home</h1>
+      </>
+    );
+
+  }
 }
 
 export default Navbar;
